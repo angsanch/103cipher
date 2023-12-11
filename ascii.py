8d8a20da22769c3c7d8f63ae0391ee16bdb2a0d1
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.edu>
2023-12-11 12:19:17 +0100

    FEAT: ascii helper

---
+++ ascii.py
@@ -0,0 +1,15 @@
+def str_to_ascii(string):
+    result = []
+    for i in string:
+        result.append(ord(i))
+    return result
+
+def ascii_to_str(asc):
+    result = ""
+    for i in asc:
+        result += chr(i)
+    return (result)
+
+def trailing_zero(short, length):
+    for i in range(length - len(short)):
+        short.append(0)
